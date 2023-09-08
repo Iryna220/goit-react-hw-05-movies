19882c1984d6e6384af84532923b5efa35da4a4c
Iryna220 <ibiriszka951@gmail.com>
2023-09-08 13:12:01 +0200

    update hw
    lazy, suspence

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,31 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import Movies from '../pages/Movies/Movies';
-import MovieDetails from '../pages/MovieDetails/MovieDetails';
-import Cast from '../pages/Cast/Cast';
-import Reviews from '../pages/Reviews/Reviews';
+import { lazy, Suspense } from 'react';
+
 import Container from 'components/Container/Container';
 import Navigation from 'components/Navigation/Navigation';
+import Loader from './Loader/Loader';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('../pages/Cast/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
 
 export const App = () => {
   return (
     <Container>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
     </Container>
   );
 };
